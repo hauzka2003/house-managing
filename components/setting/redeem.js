b1e2ef81397b329faf45f4f249ce5ae925a8b8c1
hauzka2003 <56216664+hauzka2003@users.noreply.github.com>
2022-04-02 02:20:00 +0700

    updated basic search

--- components/setting/redeem.js
+++ components/setting/redeem.js
@@ -1,9 +1,25 @@
-import styles from "./user_profile.module.css";
+import styles from "./redeem.module.css";
 import { motion } from "framer-motion";
 import { useLayout } from "../../store/layout";
+import { useState } from "react";
 
 function RedeemTab({ clicked }) {
   const { settingTabState } = useLayout();
+  const [code, setCode] = useState("");
+
+  function hyphenate(str) {
+    str = str.split("-").join("");
+    let finalStr = str?.match(/.{1,4}/g)?.join("-");
+    return finalStr;
+  }
+
+  function changeCodeHandler(e) {
+    if (e.target.value?.length >= 19) {
+      console.log(e.target.value?.length);
+      return setCode(hyphenate(e.target.value?.slice(0, 19)));
+    }
+    setCode(hyphenate(e.target.value));
+  }
 
   return (
     <motion.div
@@ -20,7 +36,35 @@ function RedeemTab({ clicked }) {
           : { x: -200, transition: { duration: 0.3 }, opacity: 0 }
       }
     >
-      redeem
+      <div className={styles.redeem_quotes}>
+        If you received our code from any source of communication, please copy
+        and paste it in below to obtain our special promotion.
+      </div>
+      <div className={styles.redeem_warning}>
+        The code is 16 characters long
+      </div>
+      <motion.input
+        className={styles.redeem_input}
+        type="text"
+        placeholder="**** - **** - **** - ****"
+        autoCapitalize="on"
+        value={code}
+        onChange={changeCodeHandler}
+        maxLength="19"
+        minLength="19"
+        onPaste={(e) => {
+          e.preventDefault();
+          let text = e.clipboardData.getData("text/plain");
+          text = text.split("-").join("");
+          text = text.slice(0, 16);
+          setCode(hyphenate(text));
+        }}
+        initial={{ boxShadow: "none" }}
+        whileFocus={{
+          boxShadow:
+            "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
+        }}
+      />
     </motion.div>
   );
 }
