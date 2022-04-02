6a1184f3f8eb80ccadd2b1e8347fdddd3af81f60
hauzka2003 <56216664+hauzka2003@users.noreply.github.com>
2022-04-02 14:03:56 +0700

    updated avatar and search functionality

--- components/icons/avatar.js
+++ components/icons/avatar.js
@@ -1,9 +1,12 @@
+import dynamic from "next/dynamic";
+
+const Avvatar = dynamic(() => import("avvvatars-react"), { ssr: false });
 import styles from "./avatar.module.css";
-import Image from "next/image";
-function AvatarUser({ style }) {
+// import Image from "next/image";
+function AvatarUser({ style, size, radius, email }) {
   return (
     <div className={styles.container} style={style}>
-      <Image src="/raiden.jpg" alt="avatar" layout="fill" />
+      <Avvatar value={email} size={size ?? 40} radius={radius} />
     </div>
   );
 }
